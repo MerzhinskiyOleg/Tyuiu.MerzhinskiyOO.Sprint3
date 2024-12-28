6a2318ce92ed157e4892dd73dc181ef9ea22477f
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 07:39:35 +0600

    Task1

---
+++ Tyuiu.MerzhinskiyOO.Sprint3.Task1.V10.Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Tyuiu.MerzhinskiyOO.Sprint3.Task1.V10.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint3.Task1.V10.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+
+        }
+    }
+}
---
+++ Tyuiu.MerzhinskiyOO.Sprint3.Task1.V10/Program.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint3.Task1.V10.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint3.Task1.V10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+          
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
