f4a7465ca297fe751898fc5c6197d106c1800124
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 07:58:52 +0600

    Task5

---
+++ Tyuiu.MerzhinskiyOO.Sprint3.Task5.V14/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint3.Task5.V14.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint3.Task5.V14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Исходные данные                                                         *");
+            Console.WriteLine("***************************************************************************");
+            int x = 5;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 3;
+            int stopValue2 = 14;
+            Console.WriteLine("Переменная x = " + x);
+            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
+            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
+            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
+            Console.WriteLine("Конец шага второй суммы ряда = " + stopValue2);
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("* Результат:                                                                   *");
+            Console.WriteLine("********************************************************************************");
+            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2));
+            Console.ReadKey();
+        }
+    }
+}
