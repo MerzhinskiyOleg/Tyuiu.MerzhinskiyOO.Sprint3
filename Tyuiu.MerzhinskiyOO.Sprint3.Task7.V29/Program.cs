8443f2525c9b088f28fb1b6c5612050afe56f367
Tyuiu.NovikovAA@outlook.com <Tyuiu.NovikovAA@outlook.com>
2024-12-28 08:13:28 +0600

    Task7

---
+++ Tyuiu.MerzhinskiyOO.Sprint3.Task7.V29/Program.cs
@@ -0,0 +1,36 @@
+﻿using Tyuiu.MerzhinskiyOO.Sprint3.Task7.V29.Lib;
+
+namespace Tyuiu.MerzhinskiyOO.Sprint3.Task7.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            DataService ds = new DataService();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            int a = -5, b = 5;
+            Console.WriteLine("Старт шага" + a);
+            Console.WriteLine("Конец шага" + b);
+            int l = ds.GetMassFunction(a, b).Length;
+            double[] r;
+            r = new double[l];
+            r = ds.GetMassFunction(a, b);
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("+----------+-----------+");
+            Console.WriteLine("|    X     |   f(x)    |");
+            Console.WriteLine("+----------+-----------+");
+            for (int i = 0; i <= l - 1; i++)
+            {
+                Console.WriteLine("|{0,5:d}     |  {1, 6:f2}   |", a, r[i]);
+                a++;
+            }
+            Console.WriteLine("+----------+-----------+");
+            Console.ReadKey();
+        }
+    }
+}
