6ef95623700dd78e03cea36e35390f84b730cac6
Egor Molodchikov <Tyuiu.MolodchikovEE@outlook.com>
2025-01-28 16:28:26 +0500

    test

---
+++ Tyuiu.MerzhinskiyOO.Sprint3.Task0.V17.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace Tyuiu.MerzhinskiyOO.Sprint3.Task0.V17.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
